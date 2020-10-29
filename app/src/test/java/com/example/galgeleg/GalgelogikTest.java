6d989180cab420eee7260fd8754e126867dfd847
mocompres <oli@live.dk>
2020-10-29 17:57:01 +0100

    Added som layout to main activity

--- app/src/test/java/com/example/galgeleg/GalgelogikTest.java
+++ app/src/test/java/com/example/galgeleg/GalgelogikTest.java
@@ -1,8 +1,8 @@
 package com.example.galgeleg;
 
-import org.junit.Test;
+        import org.junit.Test;
 
-import static org.junit.Assert.*;
+        import static org.junit.Assert.*;
 
 /**
  * Eksempel på en JUnit-test
