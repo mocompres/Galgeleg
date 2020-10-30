72670aa63393d1b5cc87d494dc11dbf9f895fab9
Oliver Comprés Poulsen <oli@live.dk>
2020-10-30 16:19:51 +0100

    made my logic in a hangman controller

---
+++ app/src/main/java/com/example/galgeleg/HangmanController.java
@@ -0,0 +1,77 @@
+package com.example.galgeleg;
+
+import java.util.ArrayList;
+
+public class HangmanController {
+    String visiableWord;
+    String wordToGuess;
+    String guessedLetters;
+    int cntWrongGuess;
+
+    HangmanController() {
+        visiableWord = "";
+        wordToGuess = "";
+        cntWrongGuess = 0;
+        guessedLetters = "";
+    }
+
+    public void startGame() {
+        // TODO: get word to guess import from a libary
+
+    }
+
+    public void guessLetter(String letter) {
+        // TODO: check if letter is correct or not
+        if (wordToGuess.contains(letter)) {
+            guessedLetters += letter;
+        }
+        else {
+            cntWrongGuess++;
+        }
+
+        constructVisiableWord();
+    }
+
+    public void constructVisiableWord() {
+        // TODO: construct visiable word
+        // use "_" if
+        visiableWord = "";
+
+        for (char c: wordToGuess.toCharArray()) {
+            if (guessedLetters.indexOf(c) != -1)
+                visiableWord += c;
+            else
+                visiableWord += "_";
+        }
+    }
+
+    public boolean isFinished() {
+        // TODO: test if game is done
+        if (cntWrongGuess < 6 && visiableWord.contains("_")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean isWon() {
+        // TODO: test is finish is won
+        if (isFinished() && visiableWord.contains("_"))
+            return false;
+        else if (isFinished() && cntWrongGuess <= 6)
+            return true;
+
+        // else
+        return false;
+    }
+
+
+    public String getVisiableWord() {
+        return visiableWord;
+    }
+
+    public String getWordToGuess() {
+        return wordToGuess;
+    }
+}
