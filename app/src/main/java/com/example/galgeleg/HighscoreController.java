f085285607833c011927ceb2cd344a9a0084c9d9
mocompres <oli@live.dk>
2020-11-03 22:53:57 +0100

    more update and clean up

--- app/src/main/java/com/example/galgeleg/HighscoreController.java
+++ app/src/main/java/com/example/galgeleg/HighscoreController.java
@@ -11,12 +11,6 @@ public class HighscoreController {
     public HighscoreController (IHighScoreData highScoreData) {
         highscoreList = new ArrayList<Score>();
 
-        highscoreList.add(new Score("Oliver", 5));
-
-        highscoreList.add(new Score("Compres", 1));
-
-        highscoreList.add(new Score("Poulsen", 3));
-
         this.data = highScoreData;
 
         highscoreList = data.getHighScoreList();
