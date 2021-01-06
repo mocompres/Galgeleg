824462cada9a4abc1fe7eb4739cf95f2ef1f3446
mocompres <oli@live.dk>
2021-01-06 21:17:21 +0100

    added sound

--- app/src/main/java/com/example/galgeleg/EndOfGameActivity.java
+++ app/src/main/java/com/example/galgeleg/EndOfGameActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,18 +26,23 @@ public class EndOfGameActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
 
         playerName = i.getStringExtra("playerName");
-
         isWon = i.getBooleanExtra("hasWon", false);
+
+        // sound init
+        MediaPlayer mpLoser = MediaPlayer.create(this,R.raw.loser);
+        MediaPlayer mpWinner = MediaPlayer.create(this,R.raw.winner);
+
         // has won
         if (isWon) {
-            currentLayout.setBackgroundColor(Color.GREEN);
+            currentLayout.setBackgroundColor(Color.GREEN); // color
+            mpWinner.start(); // sound play
             points = i.getIntExtra("printObj", 0);
-            textView.setText("Antal forsøg: " + Integer.toString(points));
+            textView.setText("Antal forsøg: " + Integer.toString(points)); // print
 
         } else { // has lost
-            currentLayout.setBackgroundColor(Color.RED);
-
-            textView.setText("Du har tabt! \n Ordet var: " + i.getStringExtra("printObj"));
+            currentLayout.setBackgroundColor(Color.RED); // color
+            mpLoser.start(); // sound play
+            textView.setText("Du har tabt! \n Ordet var: " + i.getStringExtra("printObj")); // print
 
         }
 
